172d17a92d83aff032cb4d5dfe481fbe46d91404
Abdulhadi Ahmad <get2abdul101@yahoo.com>
2021-06-19 23:11:08 +0100

    connection string updated

--- updateAdvertisement/__init__.py
+++ updateAdvertisement/__init__.py
@@ -9,7 +9,7 @@ def main(req: func.HttpRequest) -> func.HttpResponse:
 
     if request:
         try:
-            url = "mongodb://asukucosmos:UalfAgfU50qAdoywHENNddQNziXLBt4Ekue78EW9bAZpjV4DKJYgYl5CEgCMOM1zOMcnHKhnxYNBJx6GYwBH0g==@asukucosmos.mongo.cosmos.azure.com:10255/?ssl=true&replicaSet=globaldb&retrywrites=false&maxIdleTimeMS=120000&appName=@asukucosmos@"  # TODO: Update with appropriate MongoDB connection information
+            url = "mongodb://asukucosmos:MJXon0a1yPK7SSY5nDGOJYXnHVerTokpZhz7cTvJDhDcHtHQH6HhJL8nvg45tHwtRVLqsatytxvLERndK8sfyw==@asukucosmos.mongo.cosmos.azure.com:10255/?ssl=true&replicaSet=globaldb&retrywrites=false&maxIdleTimeMS=120000&appName=@asukucosmos@"  # TODO: Update with appropriate MongoDB connection information
             client = pymongo.MongoClient(url)
             database = client['asukudb']
             collection = database['advertisements']
